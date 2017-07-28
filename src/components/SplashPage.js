7ba1e5dfef7b406d90b73b65956f9243b6b36834
Alex Read <alexread730@gmail.com>
2017-07-28 14:51:28 -0600

    added splash

--- src/components/SplashPage.js
+++ src/components/SplashPage.js
@@ -8,7 +8,7 @@ class SplashPage extends Component {
   render() {
     return (
       <div>
-
+        <img className="hero-image" src="https://images.pexels.com/photos/149638/pexels-photo-149638.jpeg?w=940&h=650&auto=compress&cs=tinysrgb" alt="splash page image of Eiffel Tower"/>
       </div>
     );
   }
