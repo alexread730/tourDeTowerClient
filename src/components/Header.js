da8eb93729ef9b445199944fc379dea8d6e8b91d
Alex Read <alexread730@gmail.com>
2017-07-28 11:53:21 -0600

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
 
 const Header = (props) => {
   return(
     <header>
-
+      <Navbar toggleable>
+        <NavbarToggler />
+        <h2 href="/">Tour De Towers</h2>
+        <Collapse navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink className="right-nav" href="/components/">Book Now</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
     </header>
   );
 }
